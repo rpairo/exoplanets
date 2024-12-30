deee9f88d7d4c4b7775409126f7d286d4c3f52d1
Raul Pera Pairo <raul.perapairo@gendigital.com>
2024-12-31 00:11:17 +0100

    Updates expoplanet api

--- Sources/API/Gateway/ExoplanetAnalyzerAPI.swift
+++ Sources/API/Gateway/ExoplanetAnalyzerAPI.swift
@@ -4,26 +4,26 @@ import Composition
 import Domain
 
 public struct ExoplanetAnalyzerAPI: ExoplanetAnalyzerAPIProtocol {
-    private var presenter: ExoplanetPresenting?
+    private var presenter: ExoplanetPresenting
 
     public init() async throws {
-        let appComposition = AppComposition()
+        let appComposition: ApplicationBuilder = AppComposition()
         try await appComposition.build()
         self.presenter = try DIContainer.shared.resolve()
     }
 
     public func getOrphanPlanets() -> [ExoplanetDTO]? {
-        guard let planets = presenter?.orphanPlanets() else { return nil }
+        guard let planets = presenter.orphanPlanets() else { return nil }
         return planets.map { ExoplanetMapper.toDTO(from: $0) }
     }
 
     public func getHottestStarExoplanet() -> ExoplanetDTO? {
-        guard let exoplanet = presenter?.hottestStarExoplanet() else { return nil }
+        guard let exoplanet = presenter.hottestStarExoplanet() else { return nil }
         return ExoplanetMapper.toDTO(from: exoplanet)
     }
 
     public func getDiscoveryTimeline() -> YearlyPlanetSizeDistributionDTO? {
-        guard let timeline = presenter?.timeline() else { return nil }
+        guard let timeline = presenter.timeline() else { return nil }
         return timeline.reduce(into: [:]) { result, planetSizeCount in
             result[planetSizeCount.key] = PlanetSizeCountDTO.from(domain: planetSizeCount.value)
         }
